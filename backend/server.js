b5f43454b0653374be5b439193fc9f2853e4d6bf
Nikhil-kumar-1 <kumarnikhil787031@gmail.com>
2025-03-08 18:08:05 +0530

    Url update

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin:["https://drniren.netlify.app","http://localhost:5173/"],
+  origin:["https://doctor-niren.vercel.app","http://localhost:5173/"],
   credentials:true,
   methods:["GET", "POST","PUT","DELETE","PATCH"]
 }));
